b3961c2142b3a7a7116e15d6df25a0c9368b756c
nitin <nitingoley21@gmail.com>
2024-09-14 21:20:28 +0530

    Done first day assignment remain

--- BinarySearch/SplitSum.java
+++ BinarySearch/SplitSum.java
@@ -1,5 +1,44 @@
 package BinarySearch;
 
 public class SplitSum {
-    
+ public static void main(String[] args) {
+  
+    int arr[] =  {1,2,3,4,5};
+    int ans = SplitArray(arr, 2);
+    System.out.println(" " +ans);
+ }
+
+ static int SplitArray(int arr[] , int target) {
+
+    int s = 0;
+    int e = 0;
+    for(int i=0; i<arr.length; i++){
+        s = Math.max(s, arr[i]);
+        e += arr[i];
+    }
+    while (s<e) {
+        int mid  = s+(e-s)/2;
+        int sum =0;
+        int piece = 1;
+
+        for(int num: arr) {
+            if(sum+num >target){ 
+                sum = num;
+                piece++;
+            }
+            else {
+                sum += num;
+            }
+            if(piece>target) {
+                s = mid+1;
+            }
+            else{
+                e = mid;
+            }
+
+        }
+
+    }
+    return e;
+ }
 }
